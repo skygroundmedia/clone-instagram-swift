6f3cd727e57e5b8dfb4ffa1a6ad8874b5f9daae2
Chris Mendez <cmendez@silverbullet.local>
2016-04-17 11:50:35 -0700

    Added a Config file

---
+++ InstaClone/Config.swift
@@ -0,0 +1,22 @@
+//
+//  Config.swift
+//  InstaClone
+//
+//  Created by Chris Mendez on 4/17/16.
+//  Copyright © 2016 Chris Mendez. All rights reserved.
+//
+
+struct Config {
+    
+    //Storyboards
+    struct storyboard {
+        struct MAIN {
+            static let name = "Main"
+        }
+    }
+    
+    struct parse {
+        static let APPLICATION_ID = "PARSE_APP_ID"
+        static let CLIENT_ID      = "PARSE_CLIENT_ID"
+    }
+}

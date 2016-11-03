e01d780e19be4dae4f53e1d47f59ec0edc6d11ae
Chris Mendez <ttrojan@iMac.local>
2016-11-03 12:50:42 -0700

    removed carthage builds

--- Carthage/Checkouts/Cent/Package.swift
+++
@@ -1,7 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "Cent",
-    dependencies: [ 
-        .Package(url: "https://github.com/ankurp/Dollar", majorVersion: 5, minor: 0)
-    ])
